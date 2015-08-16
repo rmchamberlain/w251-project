4eafcd253ca2810d7710b05bf3df015a7382772a
Arthur Mak <arthur.t.mak@gmail.com>
2015-08-16 12:13:48 +0800

    rearranged folders

--- 1_load_data/get_data.sh
+++ 1_load_data/get_data.sh
@@ -1,3 +1,5 @@
+#PART 1 - GET AWS ENRON DATASET BY MOUNTING DATA ON EC2 INSTANCE
+
 # creates a 215 GiB General Purpose (SSD) volume in the Availability Zone us-east-1b for the data we use for Enron
 aws ec2 create-volume --snapshot snap-d203feb5 --size 215 --region us-east-1 --availability-zone us-east-1b --volume-type gp2
 
@@ -7,9 +9,13 @@ aws ec2 create-volume --snapshot snap-d203feb5 --size 215 --region us-east-1 --a
 #attach volume to instance
 aws ec2 attach-volume --volume-id vol-b032d05a --instance-id i-9a3d1233 --device /dev/sdf
 
-#go to key folder
+#go to directory with the .pem key and ssh into AWS
 chmod 400 w251project.pem
 ssh -i w251project.pem ubuntu@54.175.219.70
+
+#update system
+sudo apt-get update
+sudo apt-get install unzip
 
 #Use the lsblk command to view your available disk devices and their mount points (if applicable) to help you determine the correct device name to use.
 lsblk
@@ -24,3 +30,17 @@ sudo mount /dev/xvdf /data
 #check data size
 sudo du -sh /data/*
 
+#-------------------
+
+#PART 2 - SETUP SWIFT STORAGE
+
+#general setup, update, python install, swiftclient install
+sudo apt-get update
+sudo apt-get install idle
+sudo apt-get install python-pip python-dev
+sudo apt-get install -y libssl-dev libxml2-dev libxslt1-dev libssl-dev libffi-dev
+sudo apt-get install python-swiftclient
+sudo pip install --upgrade pip 
+sudo pip install --upgrade setuptools
+sudo pip install --upgrade virtualenv 
+sudo pip install python-swiftclient
