3783598b3eb36bfbd29ad5770a87658bf5724364
James <james@bla.com>
2015-08-20 18:15:53 -0700

    updated scala files, added output

--- 4_lda/src/main/scala/TopicModelling-TFIDF.scala
+++ 4_lda/src/main/scala/TopicModelling-TFIDF.scala
@@ -5,6 +5,8 @@ import org.apache.spark.SparkConf
 import org.apache.spark.mllib.clustering.LDA
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.feature.HashingTF
+import org.apache.spark.mllib.feature.IDF
 
 object TopicModel {
     def main(args: Array[String]) {
@@ -17,6 +19,8 @@ object TopicModel {
         // Load documents from text files, 1 document per file
         //val corpus: RDD[String] = sc.wholeTextFiles("hdfs://%s:54310/%s/*".format(hdfshost, hdfsroot)).map(_._2)
         val corpus: RDD[String] = sc.wholeTextFiles("hdfs://%s:9000/%s/*".format(hdfshost, hdfsroot)).map(_._2).flatMap(_.split('\n'))
+        val num_corp = corpus.count.toDouble
+        println(num_corp)
 
         // Split each document into a sequence of terms (words)
         val tokenized: RDD[Seq[String]] =
@@ -27,15 +31,17 @@ object TopicModel {
         val termCounts: Array[(String, Long)] =
             tokenized.flatMap(_.map(_ -> 1L)).reduceByKey(_ + _).collect().sortBy(-_._2)
 
+        val idfs: Map[String, Double] = tokenized.flatMap(_.map(term => (term, 1.0))).distinct.collect.toMap
+
         //for (elem <- termCounts) {
         //    println("%s, %d".format(elem._1, elem._2))
         //}
 
         //   vocabArray: Chosen vocab (removing common terms)
-        val numStopwords = 200
-        val vocabArray: Array[String] =
-            termCounts.takeRight(termCounts.size - numStopwords).map(_._1)
-        println(vocabArray.length)
+        //val numStopwords = 200
+        //val vocabArray: Array[String] =
+        //    termCounts.takeRight(termCounts.size - numStopwords).map(_._1)
+        val vocabArray: Array[String] = termCounts.map(_._1)
         //   vocab: Map term -> term index
         val vocab: Map[String, Int] = vocabArray.zipWithIndex.toMap
 
@@ -46,14 +52,14 @@ object TopicModel {
                 tokens.foreach { term =>
                     if (vocab.contains(term)) {
                         val idx = vocab(term)
-                        counts(idx) = counts.getOrElse(idx, 0.0) + 1.0
+                        counts(idx) = counts.getOrElse(idx, 0.0) + (1.0 / scala.math.log(num_corp / idfs(term)))
                       }
                 }
             (id, Vectors.sparse(vocab.size, counts.toSeq))
             }
 
         // Set LDA parameters
-        val numTopics = 100
+        val numTopics = 20
         val lda = new LDA().setK(numTopics).setMaxIterations(50)
 
         val ldaModel = lda.run(documents)
